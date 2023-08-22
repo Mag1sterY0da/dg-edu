5d0cebc0663c60da4719f3badb1bfa089d2bf834
Oleksandr Borodavchenko <oborodavchenko@gmail.com>
2023-08-22 17:00:43 +0300

    Added sample

--- src/pages/home-page/model/store.ts
+++ src/pages/home-page/model/store.ts
@@ -1,12 +1,44 @@
-import { createEvent, createStore } from 'effector';
+import { createEffect, createEvent, createStore, sample } from 'effector';
 
-export const setUsername = createEvent<string>();
-export const setPassword = createEvent<string>();
+export const usernameChanged = createEvent<string>();
+export const passwordChanged = createEvent<string>();
 
-export const $usernameStore = createStore('').on(setUsername, (_, value) => value);
-export const $passwordStore = createStore('').on(setPassword, (_, value) => value);
+export const $usernameStore = createStore('');
+export const $passwordStore = createStore('');
+
+$usernameStore.on(usernameChanged, (_, username) => username);
+$passwordStore.on(passwordChanged, (_, password) => password);
+
+export const formSubmitted = createEvent<void>();
+
+const handleSubmitFx = createEffect(
+  async ({ username, password }: { username: string; password: string }) => {
+    try {
+      await fetch('/submit', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+      });
+
+      // Handle the response
+      // ...
+    } catch (error) {
+      // Handle the error
+      // ...
+    }
+  },
+);
+
+sample({
+  clock: formSubmitted,
+  source: {
+    username: $usernameStore,
+    password: $passwordStore,
+  },
+  target: handleSubmitFx,
+});
 
 export const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
   e.preventDefault();
-  // Handle submit
+
+  formSubmitted();
 };
--- src/pages/home-page/ui/component.tsx
+++ src/pages/home-page/ui/component.tsx
@@ -1,29 +1,23 @@
 import { useStore } from 'effector-react';
 import React from 'react';
 import { Button, Container, Paper, TextField } from 'shared/ui';
-import {
-  $passwordStore,
-  $usernameStore,
-  handleSubmit as onSubmit,
-  setPassword,
-  setUsername,
-} from '../model';
+import * as model from '../model';
 
 export const HomePage = () => {
-  const username: string = useStore($usernameStore);
-  const password: string = useStore($passwordStore);
+  const username: string = useStore(model.$usernameStore);
+  const password: string = useStore(model.$passwordStore);
 
   return (
     <Container maxWidth="md">
       <Paper elevation={3} style={{ padding: '20px' }}>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={model.handleSubmit}>
           <TextField
             label="Username"
             variant="outlined"
             fullWidth
             style={{ marginBottom: '16px' }}
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => model.usernameChanged(e.target.value)}
           />
           <TextField
             label="Password"
@@ -32,7 +26,7 @@ export const HomePage = () => {
             fullWidth
             style={{ marginBottom: '16px' }}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => model.passwordChanged(e.target.value)}
           />
           <Button type="submit" variant="contained" color="primary" fullWidth>
             Log In
