b00326d712b30a3711e1678e6e6a0efe798c2b51
Oleksandr Borodavchenko <oborodavchenko@gmail.com>
2023-08-24 16:25:38 +0300

    Removed provider and created hierarchy

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,12 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import { Router } from 'pages';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { auth0Config } from 'shared/config/auth0-config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={auth0Config.domain}
-      clientId={auth0Config.clientId}
-      authorizationParams={{
-        redirect_uri: auth0Config.redirectUri,
-      }}
-    >
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </Auth0Provider>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>,
 );
--- src/pages/home-page/model/store.ts
+++ src/pages/home-page/model/store.ts
@@ -3,16 +3,15 @@ import { loginUser } from 'entities/session/model';
 
 export const usernameChanged = createEvent<string>();
 export const passwordChanged = createEvent<string>();
+export const loginUserRequested = createEvent();
+
+const loginFx = createEffect(loginUser);
 
 export const $usernameStore = createStore('');
 export const $passwordStore = createStore('');
 
 $usernameStore.on(usernameChanged, (_, username) => username);
 $passwordStore.on(passwordChanged, (_, password) => password);
-
-export const loginUserRequested = createEvent();
-
-const loginFx = createEffect(loginUser);
 
 sample({
   clock: loginUserRequested,
